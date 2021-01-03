b04162067d1e74c0c9101ef8d9f447a2dd3115d2
jessegan <jesse13579@gmail.com>
2021-01-03 17:28:35 -0500

    Change component name from Home to HomePagej

---
+++ src/client/components/Home/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import MainCard from '../Cards/MainCard'
+
+const HomePage = () => {
+  return (
+    <div className="homepage">
+      <MainCard >
+      </MainCard>
+    </div>
+  )
+}
+
+export default HomePage
