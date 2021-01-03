5710f722b8164736f8c5f58fe463db6fada2cc1d
jessegan <jesse13579@gmail.com>
2021-01-03 17:29:32 -0500

    Add HomePage and MainCard to App

--- src/client/App.js
+++ src/client/App.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
-import Home from './components/Home/Home'
+import HomePage from './components/Home/HomePage'
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route exact path='/' component={Home} />
+        <Route exact path='/' render={routerProps => (<HomePage {...routerProps} />) } />
       </Switch>
     </Router>
   );
---
+++ src/client/components/Cards/MainCard.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MainCard = ({children}) => {
+  return (
+    <div className="card-main">
+      { children }
+    </div>
+  )
+}
+
+export default MainCard
