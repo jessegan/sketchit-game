27f94f25c147bd7856fa2cf7282419da82be295a
jessegan <jesse13579@gmail.com>
2021-01-02 19:20:51 -0500

    move files into client and server folders

--- src/server/server.js
+++ src/server/server.js
@@ -3,14 +3,9 @@ const path = require('path')
 
 const app = express()
 const port = process.env.PORT || 3000
-const DIST_DIR = path.join(__dirname,'../dist')
-const HTML_FILE = path.join(DIST_DIR, 'index.html')
+const DIST_DIR = path.join(__dirname,'../../dist')
 
 app.use(express.static(DIST_DIR))
-
-app.get('/', (req,res) => {
-  res.sendFile(HTML_FILE)
-})
 
 app.listen(port, () => {
   console.log(`App listening on port: ${port}`)
