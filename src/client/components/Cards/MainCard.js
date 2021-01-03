cb65c9be4d4f35b14b61b4893c37f24ea26ab481
jessegan <jesse13579@gmail.com>
2021-01-03 18:10:22 -0500

    add styling to card-main

--- src/client/components/Cards/MainCard.js
+++ src/client/components/Cards/MainCard.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const MainCard = ({children}) => {
   return (
-    <div className="card-main">
+    <div className="card-main align-hori-center">
       { children }
     </div>
   )
