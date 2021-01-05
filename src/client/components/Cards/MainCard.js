81e18d9750d71cf3173074bd239d5150c241c506
jessegan <jesse13579@gmail.com>
2021-01-05 12:57:26 -0500

    fix: check if classes was passed down to prevent undefined from being in classname

--- src/client/components/Cards/MainCard.js
+++ src/client/components/Cards/MainCard.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const MainCard = ({classes, styles, children}) => {
   return (
-    <div className={`card-main align-hori-center ${classes}`} style={{ ...styles }} >
+    <div className={`card-main align-hori-center ${classes || ""}`} style={{ ...styles }} >
       { children }
     </div>
   )
