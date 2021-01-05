ed1182159bb5cb51960179fa8c9a178a589f1c9d
jessegan <jesse13579@gmail.com>
2021-01-04 20:29:11 -0500

    Create lobby button now creates new lobby and redirects

--- src/client/components/Home/HomePage.js
+++ src/client/components/Home/HomePage.js
@@ -3,13 +3,22 @@ import BlockButton from '../Buttons/BlockButton'
 import MainCard from '../Cards/MainCard'
 import JoinLobby from './JoinLobby'
 
+import { createLobby } from '../../networking'
+
 const HomePage = (props) => {
+
+  const onClick = () => {
+    createLobby()
+      .then(resp => resp.json())
+      .then(data => props.history.push(`/lobby/${data.code}`))
+  }
+
   return (
     <div className="homepage">
       <MainCard >
         <JoinLobby history={ props.history } />
         <p className="text-center">----</p>
-        <BlockButton className={"btn-secondary"} text={"Create Lobby"} />
+        <BlockButton className={"btn-secondary"} text={"Create Lobby"} onClick={ onClick } />
       </MainCard>
     </div>
   )
