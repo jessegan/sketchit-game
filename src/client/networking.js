1c2f115919803d80b9e79eda2f1cb7ae2602b683
jessegan <jesse13579@gmail.com>
2021-01-04 20:28:10 -0500

    added createLobby function

--- src/client/networking.js
+++ src/client/networking.js
@@ -1,3 +1,14 @@
 import io from 'socket.io-client'
 
 export const socket = io(`ws://${window.location.host}`, { reconnection: false })
+
+export const createLobby = () => {
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }
+
+  return fetch(`http://${window.location.host}/lobbies`, options)
+}
