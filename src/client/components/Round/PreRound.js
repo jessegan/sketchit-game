fd4589d3d4b5957fffbc94d2c5bb3e942e79f90a
jessegan <jesse13579@gmail.com>
2021-01-17 15:38:09 -0500

    Add: Create PreRound component that shows round starting message

---
+++ src/client/components/Round/PreRound.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PreRound = ({ current_round }) => {
+  return (
+    <div className={"pre-round"}>
+      <h1>Round { current_round} Starting Soon...</h1>
+    </div>
+  )
+}
+
+export default PreRound
