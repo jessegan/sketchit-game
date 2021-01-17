1d4efa528e341d15815051a341a2e3542501a439
jessegan <jesse13579@gmail.com>
2021-01-17 15:32:52 -0500

    Update: Game now renders based on its status

--- src/client/components/Game/Game.js
+++ src/client/components/Game/Game.js
@@ -2,59 +2,84 @@ import React, { Component } from 'react'
 import MainCard from '../Cards/MainCard'
 
 import { subscribeToGame } from '../../networking'
-import ScoresList from '../Players/ScoresList'
-import Header from './Header'
-import Content from './Content'
-import Chat from './Chat'
+import InGame from './InGame'
 
 export class Game extends Component {
 
-  // state = {
-  //   status: "GAME_LOADING",
-  //   round: null,
-  //   turn: null
-  // }
-
-  // TESTING
   state = {
-    status: "IN_TURN",
-    round: {
-      number: 1
-    },
-    turn: {
-      drawing_player: {
-        socketid: 1,
-        username: "test1"
-      }
-    },
-    scores: {
-      1: 1000,
-      2: 300,
-      3: 400,
-      4: 0
-    }
+    status: "GAME_LOADING",
+    scores: {},
+    current_round: null,
+    round: null
   }
 
+  // TESTING
+  // state = {
+  //   status: "IN_GAME",
+  //   scores: {
+  //     "1": 1000,
+  //     "2": 300,
+  //     "3": 400,
+  //     "4": 0
+  //   },
+  //   current_round: 1,
+
+  //   round: {
+  //     status: "POST_ROUND",
+  //     standings: ["1","2","3","4"]
+
+  //     // turn: {
+  //     //   status: "POST_TURN",
+  //     //   drawing_player: "3",
+  //     //   word: "apple",
+  //     //   points: [
+  //     //     ["4", 500],
+  //     //     ["2", 400],
+  //     //     ["3", 300],
+  //     //     ["1", 0],
+  //     //   ]
+  //     // }
+
+  //   }
+
+  // }
+
   // Remove for testing without socket updates
-  // componentDidMount() {
-  //   subscribeToGame(this.updateGame)
+  componentDidMount() {
+    subscribeToGame(this.updateGame)
+  }
+
+  // componentWillUnmount() {
   // }
 
   updateGame = (gameData) => {
+
+    console.log(gameData)
+
     this.setState({
-      ...gameData,
-      round: gameData.round,
-      turn: gameData.turn
+      status: gameData.status,
+      current_round: gameData.current_round,
+      scores: gameData.scores,
+      round: gameData.round
     })
+  }
+
+  renderContent() {
+    switch(this.state.status) {
+      case ("IN_GAME"):
+        return (
+          <InGame players={ this.props.players } scores={ this.state.scores } current_round={ this.state.current_round } round= { this.state.round } />
+        )
+      case("END_GAME"):
+      default:
+        return <></> // TODO: Add error page
+    }
   }
   
   render() {
     return (
-      <MainCard classes={"game-container align-hori-center grid"}>
-        <Header status={ this.state.status } round={ this.state.round } turn={ this.state.turn } />
-        <ScoresList players={ this.props.players } scores={ this.state.scores } />
-        <Content status={ this.state.status } round={ this.state.round } />
-        <Chat />
+      <MainCard classes={"game-container align-hori-center"}>
+        { this.renderContent() }
       </MainCard>
 
     )
