9f58308c5803d205454477cd203ebd0cbcfd8a55
jessegan <jesse13579@gmail.com>
2021-01-21 16:52:29 -0500

    Add: Add connect redux to react

--- src/client/index.js
+++ src/client/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import rootReducer from './reducers/root'
+import thunk from 'redux-thunk'
 
 import App from './App'
 
 import { connect } from './networking'
 
+const store = createStore(rootReducer, applyMiddleware(thunk))
+
 Promise.all([
   connect()
 ]). then(() => {
   ReactDOM.render(
-    <React.StrictMode>
+    <Provider store={ store }>
       <App />
-    </React.StrictMode>,
+    </Provider>
+    ,
     document.getElementById('root')
   );
 })
