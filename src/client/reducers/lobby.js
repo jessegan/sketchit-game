4d97748ddb42c89f24ae39d66552beeec9c43ff4
jessegan <jesse13579@gmail.com>
2021-01-21 16:52:05 -0500

    Add: Add redux reducers for lobby and root

---
+++ src/client/reducers/lobby.js
@@ -0,0 +1,23 @@
+import { getSocketId } from '../networking'
+
+export default function lobby(state={
+  userId: getSocketId(),
+  code: null,
+  playerCreated: false,
+  status: "LOADING",
+  players: null,
+  host: null
+},action){
+
+  switch(action.type){
+    case("UPDATE_LOBBY"):
+      return Object.assign({}, state, action.payload)
+    case("JOIN_LOBBY"):
+      return Object.assign({}, state, {
+        playerCreated: true
+      })
+    case("LEAVE_LOBBY"):
+    default:
+      return state
+  }
+}
---
+++ src/client/reducers/root.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux'
+import lobby from './lobby'
+
+export default combineReducers({
+  lobby
+})
