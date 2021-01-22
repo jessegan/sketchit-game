67f898733ffd082f3462d3f7c6df987e4dc73dec
jessegan <jesse13579@gmail.com>
2021-01-22 11:16:17 -0500

    Update: Move initialization of store into its own file

--- src/client/index.js
+++ src/client/index.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
-import rootReducer from './reducers/root'
-import thunk from 'redux-thunk'
+import store from './store'
 
 import App from './App'
 
 import { connect } from './networking'
-
-const store = createStore(rootReducer, applyMiddleware(thunk))
 
 Promise.all([
   connect()
