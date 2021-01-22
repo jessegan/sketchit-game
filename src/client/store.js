5c6f782156f72b979f6d334d7a493c1fee1c93f4
jessegan <jesse13579@gmail.com>
2021-01-22 12:48:35 -0500

    Add: create store with redux devtools extension

--- src/client/store.js
+++ src/client/store.js
@@ -1,7 +1,9 @@
-import { createStore, applyMiddleware }  from 'redux'
+import { createStore, applyMiddleware, compose }  from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
 import thunk from 'redux-thunk'
 import rootReducer from './reducers/root'
 
-const store = createStore(rootReducer, applyMiddleware(thunk))
+const composedEnhancer = composeWithDevTools(applyMiddleware(thunk))
+const store = createStore(rootReducer, composedEnhancer)
 
 export default store
