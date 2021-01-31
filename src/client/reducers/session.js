7c3f90d3d9b6dc3352e98e122b6dec9b60d01a72
jessegan <jesse13579@gmail.com>
2021-01-31 16:30:57 -0500

    Add: Add initial testing state

--- src/client/reducers/session.js
+++ src/client/reducers/session.js
@@ -1,10 +1,18 @@
-export default function session(state={
+const initialState = {
   userId: null
-}, action) {
+}
+
+export default function session(state=initialState, action) {
   switch(action.type) {
     case 'session/connect':
       return Object.assign({}, state, action.payload)
     default:
       return Object.assign({}, state)
   }
+}
+
+/* TESTING STATE */
+
+const initialTestState = {
+  userId: "1"
 }
