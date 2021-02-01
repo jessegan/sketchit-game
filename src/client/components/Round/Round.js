afd2ef31278ccf8e03bfa922fcd56ae7da31e5dc
jessegan <jesse13579@gmail.com>
2021-02-01 14:28:07 -0500

    Update: remove props because changing to redux

--- src/client/components/Round/Round.js
+++ src/client/components/Round/Round.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
 import InRound from './InRound'
 import PostRound from './PostRound'
 import PreRound from './PreRound'
@@ -6,13 +8,13 @@ import PreRound from './PreRound'
 export class Round extends Component {
 
   renderContent() {
-    switch(this.props.round.status) {
+    switch(this.props.status) {
       case("PRE_ROUND"):
         return (<PreRound current_round={ this.props.current_round } />)
       case("IN_ROUND"):
-        return (<InRound turn={ this.props.round.turn } players={ this.props.players } />)
+        return (<InRound />)
       case("POST_ROUND"):
-        return (<PostRound players={ this.props.players } current_round={ this.props.current_round } scores={ this.props.scores } standings={ this.props.round.standings }/>)
+        return (<PostRound players={ this.props.players } current_round={ this.props.current_round } scores={ this.props.scores } standings={ this.props.standings }/>)
     }
   }
 
@@ -25,4 +27,14 @@ export class Round extends Component {
   }
 }
 
-export default Round
+function mapStateToProps(state) {
+  return {
+    status: state.game.round.status,
+    current_round: state.game.current_round,
+    standings: state.game.round.standings,
+    scores: state.game.scores,
+    players: state.lobby.players
+  }
+}
+
+export default connect(mapStateToProps)(Round)
