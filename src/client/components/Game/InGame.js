756d3e97abaa97835714be70e840a58b794d84e1
jessegan <jesse13579@gmail.com>
2021-02-01 14:27:04 -0500

    Update: remove props because using redux

--- src/client/components/Game/InGame.js
+++ src/client/components/Game/InGame.js
@@ -2,11 +2,11 @@ import React from 'react'
 import ScoresList from '../Players/ScoresList'
 import Round from '../Round/Round'
 
-const InGame = ({ players, scores, current_round, round }) => {
+const InGame = () => {
   return (
     <div className="in-game-container grid">
-      <ScoresList players={ players } scores={ scores } round={ round } />
-      <Round round={ round } scores={ scores } players={ players } current_round={ current_round }/>
+      <ScoresList />
+      <Round />
     </div>
   )
 }
